eb1d2435f23800270d39efb62f98337027231f9b
Ludwig Schubert <ludwigschubi1@gmail.com>
2026-02-10 09:19:35 +0800

    fix(deploy) chart

--- chart/scripts/deploy.sh
+++ chart/scripts/deploy.sh
@@ -7,8 +7,8 @@ set -e
 : "${NAMESPACE:?Environment variable NAMESPACE is required}"
 : "${RELEASE_NAME:?Environment variable RELEASE_NAME is required}"
 : "${RELEASE_TAG:?Environment variable RELEASE_TAG is required}"
-: "${NFS_SERVER:?Environment variable NFS_SERVER is required}"
-: "${NFS_PATH:?Environment variable NFS_PATH is required}"
+: "${DO_NFS_SERVER:?Environment variable DO_NFS_SERVER is required}"
+: "${DO_NFS_PATH:?Environment variable DO_NFS_PATH is required}"
 
 helm upgrade $RELEASE_NAME chart \
   --install \
